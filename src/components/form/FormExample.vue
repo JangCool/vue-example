e84556268940bb5cb864e21071cee46ebfdf7d32
zerocooldog <zerocooldog@zen9.co.kr>
2020-06-18 21:48:05 +0900

    예제8

--- src/components/form/FormExample.vue
+++ src/components/form/FormExample.vue
@@ -1,5 +1,48 @@
 <template>
-    <input v-bind:value="name" v-on:input="events.updateName">
+    <div>
+        <input v-bind:value="name" v-on:input="events.updateName">
+        <br><br>
+
+        <input type="checkbox" id="checkbox" v-model="data.checked">
+        <label for="checkbox">{{ data.checked }}</label>
+        <br><br>
+
+        <input type="checkbox" id="jack" value="Jack" v-model="data.checkedNames">
+        <label for="jack">Jack</label>
+        <input type="checkbox" id="john" value="John" v-model="data.checkedNames">
+        <label for="john">John</label>
+        <input type="checkbox" id="mike" value="Mike" v-model="data.checkedNames">
+        <label for="mike">Mike</label>
+        <br><br>
+
+        <span>체크한 이름: {{ data.checkedNames }}</span><br>
+
+        <input type="radio" id="one" value="One" v-model="data.picked">
+        <label for="one">One</label>
+        <br>
+        <input type="radio" id="two" value="Two" v-model="data.picked">
+        <label for="two">Two</label>
+        <br>
+        <span>선택: {{ data.picked }}</span>
+        <br><br>
+
+        <select v-model="data.selected">
+        <option disabled value="">Please select one</option>
+        <option value="avalue">A</option>
+        <option>B</option>
+        <option>C</option>
+        </select>
+        <span>선택함: {{data.selected }}</span>
+        <br><br>
+
+        <select v-model="data.selected">
+            <option v-for="option in data.options" v-bind:key="option.value">
+                {{ option.text }}
+            </option>
+        </select>
+        <span>Selected: {{ selected }}</span>
+
+    </div>
 </template>
 
 <script>
@@ -9,7 +52,16 @@ export default {
   name: 'form-example',
   setup (props) {
     const data = reactive({
-      name: ''
+      name: '',
+      checked: false,
+      checkedNames: [],
+      picked: '',
+      selected: '',
+      options: [
+        { text: 'One', value: 'A' },
+        { text: 'Two', value: 'B' },
+        { text: 'Three', value: 'C' }
+      ]
     })
 
     const updateName = (event) => {
