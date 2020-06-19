cb32a93d8ada30c2712fd0cfd364d51ef953ae8b
장진철(zerocooldog@pionnet.co.kr) <zerocooldog@pionnet.co.kr>
2020-06-19 17:41:04 +0900

    directive 커밋

--- src/main.js
+++ src/main.js
@@ -2,12 +2,10 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import { store } from '@/store'
+import DirectiveFocus from '@/directives/DirectiveFocus.js'
 
 createApp(App)
   .use(router)
   .use(store)
-  .directive('focus', function (el, binding) {
-    console.log(binding)
-    el.focus()
-  })
+  .directive('focus', DirectiveFocus)
   .mount('#app')
